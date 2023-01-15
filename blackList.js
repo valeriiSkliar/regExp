425308a5ec549288d5f1dee9d8ec7afc902a98ea
DeepWeep <91907727+DeepWeep@users.noreply.github.com>
2023-01-15 02:26:18 +0100

    fully complete create and hide black list buttons

--- blackList.js
+++ blackList.js
@@ -4,8 +4,9 @@ let surname = ['Lom', 'Perker', 'Dalas', 'Manson', 'Cree'];
 let firedSurname1;
 let firedSurname2;
 let firedSurname3;
+let fired = '';
 
-function getRandomSurname() {
+function getRandomSurname() { //Получаем три рандомные фамилии на увольнение
     function rand() {
         return Math.floor(Math.random() * 5);
     }
@@ -13,12 +14,14 @@ function getRandomSurname() {
     firedSurname2 = surname[rand()];
     firedSurname3 = surname[rand()];
 }
-
 getRandomSurname();
+console.log(firedSurname1)
+console.log(firedSurname2)
+console.log(firedSurname3)
 
 let blackList = [firedSurname1, firedSurname2, firedSurname3, 'hamster', '€', '£']
 
-for (let i = 0; i < workersPars.length; i += 1) {
+for (let i = 0; i < workersPars.length; i += 1) { //Поиск людей для увольнения
     if (workersPars[i].surname === blackList[0] || workersPars[i].surname === blackList[1] || workersPars[i].surname === blackList[2]) {
         console.log(workersPars[i].surname)
     } else if (workersPars[i].curency === blackList[4] || workersPars[i].curency === blackList[4]) {
@@ -27,3 +30,43 @@ for (let i = 0; i < workersPars.length; i += 1) {
         console.log(workersPars[i].pets)
     }
 };
+
+
+function allFired() { //Функция выводит фамилии людей для увольнения в черный список, без лишних повторов фамилий, если срандомило одинаковые
+    fired += firedSurname1;
+    if (!(firedSurname1 === firedSurname2)) {
+        fired = fired + ' ' + firedSurname2
+    }
+    if (!(firedSurname1 === firedSurname3)) {
+        fired = fired + ' ' + firedSurname3
+    }
+    if (firedSurname2 === firedSurname3) {
+        fired = fired.replace(firedSurname3, '')
+    }
+    if (firedSurname1 === firedSurname2 && firedSurname1 === firedSurname3 && firedSurname2 === firedSurname3) {
+        fired = firedSurname1;
+    }
+    return fired;
+}
+
+let blackListSurname = 'Black List<br>Surname: ' + allFired() + ';<br>People who get paid in: €, £;<br> People with that pet: hamster.'
+let count = 0;
+document.querySelector('.blackList').addEventListener('click', () => { //Реализация кнопки, что создает черный список
+    let list;
+    if(count === 0){
+    list = document.createElement('div');
+    function create() {
+        list.classList.add('list');
+    }
+    create();
+        list.innerHTML = blackListSurname;
+        list.classList.add('displayed');
+
+    body.append(list);
+    count = 1;
+}
+    document.querySelector('.hide').addEventListener('click', () => { //Кнопка, что прячет черный список
+        list.classList.add('nonDisplayed')
+        count = 0;
+    })
+})
